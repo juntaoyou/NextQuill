199c25eb3193d7241041f11634cd827658d4fc99
Juntao You <63189881+juntaoyou@users.noreply.github.com>
2025-05-22 16:50:08 +0800

    Update create-dataset.py

--- create-dataset.py
+++ create-dataset.py
@@ -1,7 +1,7 @@
 import os
 import sys
-os.environ["HF_DATASETS_CACHE"] = '/NAS/yjt/hf_dataset2'
-os.environ['HF_HOME'] = '/NAS/yjt/datasets'
+os.environ["HF_DATASETS_CACHE"] = ''
+os.environ['HF_HOME'] = ''
 os.environ["CUDA_VISIBLE_DEVICES"] = '3'
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if project_root not in sys.path:
@@ -17,28 +17,28 @@ set_seed(42)
 
 categories = ["Books","Movies_and_TV", "CDs_and_Vinyl"]
 
-# train_dataset1 = load_from_disk("/NAS/yjt/demo-rag/data/dataset/dataset_train_Books")
-# train_dataset2 = load_from_disk("/NAS/yjt/demo-rag/data/dataset/dataset_train_CDs_and_Vinyl")
-# train_dataset3 = load_from_disk("/NAS/yjt/demo-rag/data/dataset/dataset_train_Movies_and_TV")
+# train_dataset1 = load_from_disk("./data/dataset/dataset_train_Books")
+# train_dataset2 = load_from_disk("./data/dataset/dataset_train_CDs_and_Vinyl")
+# train_dataset3 = load_from_disk("./data/dataset/dataset_train_Movies_and_TV")
 # train_dataset = concatenate_datasets([train_dataset1, train_dataset2, train_dataset3])
 # # train_dataset.save_to_disk("./data/dataset_train_cft")
 
-# val_dataset1 = load_from_disk("/NAS/yjt/demo-rag/data/dataset/dataset_test_Books")
-# val_dataset2 = load_from_disk("/NAS/yjt/demo-rag/data/dataset/dataset_test_CDs_and_Vinyl")
-# val_dataset3 = load_from_disk("/NAS/yjt/demo-rag/data/dataset/dataset_test_Movies_and_TV")
+# val_dataset1 = load_from_disk("./data/dataset/dataset_test_Books")
+# val_dataset2 = load_from_disk("./data/dataset/dataset_test_CDs_and_Vinyl")
+# val_dataset3 = load_from_disk("./data/dataset/dataset_test_Movies_and_TV")
 # val_dataset = concatenate_datasets([val_dataset1, val_dataset2, val_dataset3])
 # val_dataset.save_to_disk("./data/dataset_test_with_user_ids")
 
-meta_dataset1 = load_from_disk("/NAS/yjt/demo-rag/data/dataset/dataset_meta_Books")
-meta_dataset2 = load_from_disk("/NAS/yjt/demo-rag/data/dataset/dataset_meta_CDs_and_Vinyl")
-meta_dataset3 = load_from_disk("/NAS/yjt/demo-rag/data/dataset/dataset_meta_Movies_and_TV")
+meta_dataset1 = load_from_disk("./data/dataset/dataset_meta_Books")
+meta_dataset2 = load_from_disk("./data/dataset/dataset_meta_CDs_and_Vinyl")
+meta_dataset3 = load_from_disk("./data/dataset/dataset_meta_Movies_and_TV")
 meta_dataset = concatenate_datasets([meta_dataset1, meta_dataset2, meta_dataset3])
 
 meta_dataset = dict(zip(meta_dataset["asin"],
                         zip(meta_dataset["title"],
                             meta_dataset["description"])))
 
-llm_model_name = "/NAS/yjt/Qwen2.5-3B"
+llm_model_name = "Qwen/Qwen2.5-3B"
 llm_tokenizer = AutoTokenizer.from_pretrained(llm_model_name)
 llm_tokenizer.padding_side = "left"
 
@@ -58,7 +58,7 @@ llm_tokenizer.padding_side = "left"
 # hf_dataset.save_to_disk("./data/dataset_val")
 
 for i, category in enumerate(categories):
-    test_main_dataset = load_from_disk(f"/NAS/yjt/demo-rag/data/dataset/dataset_test_{category}")
+    test_main_dataset = load_from_disk(f"./data/dataset/dataset_test_{category}")
     test_dataset = CFTDataset(
         test_main_dataset, 
         meta_dataset,
@@ -77,17 +77,3 @@ for i, category in enumerate(categories):
 # hf_dataset = convert_to_dataset(personal_dataset)
 # hf_dataset.save_to_disk("./data/dataset_test")
 
-# for i, category in enumerate(categories):
-#     test_main_dataset = load_dataset(
-#         "SnowCharmQ/DPL-main",
-#         category,
-#         split="test"
-#     ).map(lambda _: {"category": category})
-#     test_dataset = PersonalDataset(
-#         test_main_dataset, 
-#         meta_dataset,
-#         llm_tokenizer=llm_tokenizer,
-#         training=False
-#     )
-#     hf_dataset = convert_to_dataset(test_dataset)
-#     hf_dataset.save_to_disk(f"data/dataset_test_{category}")
